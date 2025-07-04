f3b358703fe5ba836dc1cf17f0cf16f970972148
msjdyyyydhskak <rashupanel@gmail.com>
2025-07-05 00:50:19 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "RASHU-MD=Aj8ywbCI#4Dm6IlWzpw_FRHRMbeglqZb9l4enuGAXdR00eyIH1x8",
+SESSION_ID: process.env.SESSION_ID || "RASHU-MD=g30lnJ7Q#yymtbeUxt47E5tJyxL8jZ5eAXngPPyjpSfxjs-Iy7kU",
 PREFIX: process.env.PREFIX || '.' ,
 MODE: process.env.MODE === undefined ?"public" : process.env.MODE,
 AUTO_VOICE: process.env.AUTO_VOICE === undefined ? "false" : process.env.AUTO_VOICE,
